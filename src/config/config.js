447ff852ec06bfc291abd88f3693c4e37d2fedbf
Jorge Banegas <jorge.g.banegas@gmail.com>
2023-08-13 13:02:26 -0400

    chore: actualizar config

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
-const apiUrl = 'localhost';
-const apiEmployerUrl = 'localhost';
-const apiGeofencingUrl= 'localhost'
-const googleMapsApiKey = 'google-apy-key';
+const apiUrl = 'https://p01--auth-microservices--9wpsqj5d69c5.code.run/api/v1';
+const apiEmployerUrl = 'http://104.45.156.208/api/v1';
+const apiGeofencingUrl= 'http://35.192.97.248/api/v1'
+const googleMapsApiKey = 'AIzaSyDe6M3BlhqSlT63gUqEsElF3QnQMzKodPA';
 
 export default {
     apiUrl,
